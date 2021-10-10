fad6f95c22064ec955b2cb8b98bce1f92bd14b77
tariq3455 <trkysl55@gmail.com>
2021-10-10 17:24:32 +0300

    flask-03-iffor added

---
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -0,0 +1,27 @@
+from logging import root
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    first = "this is my first condition"
+    return render_template("index.html", message=first)
+
+@app.route("/tariq")
+def mylist():
+    names = ["mike", "james", "bilbao", "luca"]
+    return render_template("body.html", object=names)
+
+
+
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    app.run(debug=True)
